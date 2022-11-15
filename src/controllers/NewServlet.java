6f59c732f2646a442b008a05a9c09139462b6b83
higuchiyumiko <101847022+higuchiyumiko@users.noreply.github.com>
2022-11-15 14:28:52 +0900

    Initial commit

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+//新規登録
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em=DBUtil.createEntityManager();
+		em.getTransaction().begin();
+
+		//CSRF対策
+		request.setAttribute("_token",request.getSession().getId());
+
+		//おまじないとしてのインスタンスを生成
+		request.setAttribute("message",new Message());
+
+		RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+		rd.forward(request, response);
+
+	}
+
+}
